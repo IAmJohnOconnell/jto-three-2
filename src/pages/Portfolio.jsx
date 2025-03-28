842c89c59d81bffe8606527dd7612077014ce326
John <johnathano619@gmail.com>
2025-03-28 11:05:31 -0400

    updated imports for netlify

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import Navbar from "../components/Nav";
 import styled from "styled-components";
 import PortfolioCard from "../components/PortfolioCard";
-import f1HubThumb from "../assets/portfolioImages/f1HubThumb.png";
-import eOneThumb from "../assets/portfolioImages/eOneThumb.png";
-import meevoThumb from "../assets/portfolioImages/meevoThumb.png";
-import pokedexThumb from "../assets/portfolioImages/pokedexThumb.png";
-import flwpThumb from "../assets/portfolioImages/flwpThumb.png";
 
 const StyledPortfolio = styled.div`
   max-width: 1200px;
@@ -94,22 +89,22 @@ const Portfolio = () => {
           <div className="portfolioCards">
             <PortfolioCard
               title={"Formula 1 Hub"}
-              icon={f1HubThumb}
+              icon={"f1hubThumb.png"}
               url={"https://formula1hub.netlify.app/"}
             />
             <PortfolioCard
               title={"Meevo"}
-              icon={meevoThumb}
+              icon={"meevoThumb.png"}
               url={"https://www.meevo.com/"}
             />
             <PortfolioCard
               title={"E-One"}
-              icon={eOneThumb}
+              icon={"eoneThumb.png"}
               url={"https://e-one.com/vector/"}
             />
             <PortfolioCard
               title={"Pokedex"}
-              icon={pokedexThumb}
+              icon={"pokedexThumb.png"}
               url={"https://pokedexv2-iamjoconnell.netlify.app/"}
             />
           </div>
@@ -126,7 +121,7 @@ const Portfolio = () => {
           <div className="portfolioCards">
             <PortfolioCard
               title={"Florida Water Professionals"}
-              icon={flwpThumb}
+              icon={"flwpThumb.png"}
               url={
                 "https://www.figma.com/proto/rBBvw7ghNixZtTZ2VZhbnV/ServiceStream?node-id=390-3599&t=o9PgFbLIWoqJjxXu-1&scaling=min-zoom&content-scaling=fixed"
               }
